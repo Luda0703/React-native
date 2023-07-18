875b512845c0315f71b0ff54c9b58575d7a2c9e3
Nikitenko Ludmila <117203301+Luda0703@users.noreply.github.com>
2023-07-18 10:47:38 +0300

    update

--- Screen/mainScreen/CreatePostsScreen.jsx
+++ Screen/mainScreen/CreatePostsScreen.jsx
@@ -1,10 +1,45 @@
-import { Text, View, StyleSheet } from "react-native";
-import React from "react";
+import { 
+    Text,
+    View, 
+    StyleSheet, 
+    TouchableOpacity,
+    Image,
+ } from "react-native";
+import React, {useState} from "react";
+import { Camera } from "expo-camera";
+// import axios from 'axios';
 
 export const CreatePostsScreen = () => {
+    const [camera, setCamera] = useState(null);
+    const [photo, setPhoto] = useState(null);
+
+    const takePhoto = async () => {
+      const photo = await camera.takePictureAsync();
+      setPhoto(photo.url);
+      console.log('photo', photo);
+    }
+
   return (
     <View style={styles.container}>
-      <Text>CreatePostsScreen</Text>
+      <Camera 
+      style={styles.camera}
+      ref={setCamera}
+      >
+        {photo &&
+        <View style={styles.takePhotoContainer}>
+        <Image 
+        source={{url: photo}} 
+        // style={{height: 200, width: 200}}
+        />
+        </View>
+        }
+        <TouchableOpacity
+        style={styles.snapContainer} 
+        onPress={takePhoto}>
+            <Text style={styles.snap}>SNAP</Text>
+        </TouchableOpacity>
+      </Camera>
+    
     </View>
   );
 };
@@ -12,7 +47,33 @@ export const CreatePostsScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
   },
+  camera: {
+    height: 300,
+    marginTop: 50,
+    alignItems: 'center',
+  },
+  snap: {
+    color: '#fff'
+  },
+  snapContainer: {
+    marginTop: 200,
+    borderWidth: 1,
+    borderColor: '#E5E5E5',
+    width: 70,
+    height: 70,
+    borderRadius: "50%",
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  takePhotoContainer: {
+    position: 'absolute',
+    top: 50,
+    left: 10,
+    borderColor: '#E5E5E5',
+    borderWidth: 1,
+    width: 200,
+    height: 200, 
+    
+  }
 });
